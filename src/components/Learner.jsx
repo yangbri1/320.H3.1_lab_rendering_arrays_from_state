f18720fd97fa3049c88c7dacf312d5f2a2ada90c
brian <yangbri1@msu.edu>
2024-10-28 22:07:14 -0400

    thinking about import <Score> component into <Learner> component to format then import <Learner> to <AppComp> component, finally <AppComp> to root App.jsx component to render -- can't nest components?

--- src/components/Learner.jsx
+++ src/components/Learner.jsx
@@ -1,9 +1,24 @@
 // import given Learner data -- array of Learner objects
 import { dataLearner } from "../utilities/data.mjs";
 
-// creating React function component
-function Learner({ learner }){
+// import <Score> component to be used in formatting/renf in <Learner> component
+import Score from "./Score.jsx";
+
+// creating React function component -- user defined components must be Capitalized in React otw not detected
+export default function Learner(){
+    // .map() method creates a new array by applying a function/condition to old array dataLearner
+    // conventionally anonymous arrow function
+    const listLearners = dataLearner.map((student) => {
+        // let key to be student.name to rid warning in console -- normally with an unique ObjectId "_id"
+        <li key={student.name}>
+            <p><strong>Name: </strong> {student.name} </p>
+            <p><strong>Bio: </strong> {student.bio} </p>
+        </li>
+        // <strong> tag w/ backticks probably should be wrapped with {}? in .jsx
+        // https://stackoverflow.com/questions/70574941/add-strong-tag-within-jsx-with-backticks#:~:text=%7B%20%60Your%20list%20contains%20%60%7D%20%3Cstrong%3E%7BArray.length%7D%20%3C%2Fstrong%3E%20%7B%60row%28s%29.%60%7D,a%20backtick%20will%20be%20translated%20into%20a%20string.
     
+    });
+    return(<ul>{listLearners}</ul>);
 }
 
-export default Learner;
+// export default Learner; // "export default" prefix already added to Learner() function above
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -14,7 +14,15 @@ import { dataLearner } from "../utilities/data.mjs";
 // }
 
 function Score(){
-    
+    // implement JS array .map() method on dataLearner old array & search for learner scores' date & score
+    // here we using nameless function declaration 
+    const listScores = dataLearner.map(function(student){
+        // refer to dataLearner array collection & dot notation call for respective values
+        <li key={student.name}>
+            <p>{student.scores}</p>
+        </li>
+    });
+    return(<ul>{listScores}</ul>);
 }
 
 export default Score;
